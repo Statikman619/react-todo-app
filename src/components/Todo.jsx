98aa7bb7dfd4c6cdb13b24071335f688f6c64d21
Statikman619 <martinez.michael02@gmail.com>
2022-04-28 17:55:41 -0700

    started my return function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,3 +2,11 @@ import React from "react";
 import { Card, CardContent, Typography, Container } from "@material-ui/core";
 import react from "react";
 import { Card, CardContent, Typography, Container } from "@material-ui/core";
+
+const Todo = ({ todo, todoNo }) => {
+  return (
+    <Container>
+      <Card></Card>
+    </Container>
+  );
+};
