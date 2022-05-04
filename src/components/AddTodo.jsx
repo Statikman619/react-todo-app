36856b60135d620e8583156821641e586d9977f3
Statikman619 <martinez.michael02@gmail.com>
2022-05-03 17:49:39 -0700

    added my first AddTodo const

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { FormControl, Container, Button, TextField } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
 
+const AddTodo = ({ makeTodos }) => {
+  const [text, setText] = useState("");
+};
+
 function AddTodo() {
   return <div>AddTodo</div>;
 }
