511652ded02de3b854e3e73f6c4956810d952797
Statikman619 <martinez.michael02@gmail.com>
2022-05-12 19:26:29 -0700

    trying to fix my todos

--- src/components/Todos/AddTodo.jsx
+++ src/components/Todos/AddTodo.jsx
@@ -1,24 +1,34 @@
-import React, { useState } from "react";
-import { FormControl, Container, Button, TextField } from "@material-ui/core";
+import { useState, FC, ChangeEvent } from "react";
+import {
+  FormControl,
+  Container,
+  Button,
+  TextField,
+  Snackbar,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import { Add } from "@material-ui/icons";
 
-const AddTodo = ({ makeTodos }) => {
+const AddTodo: FC<{ addTodo: (text: string) => void }> = ({ addTodo }) => {
   const [text, setText] = useState("");
-
-  const handleChange = (e) => setText(e.target.value);
-  const createTodo = (e) => {
+  const [open, setOpen] = useState(false);
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => setText(e.target.value);
+  const createTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    addTodo(text);
     setText("");
-    makeTodos(text);
+    if (text.trim()) setOpen(true);
   };
 
   return (
     <div>
       <Container maxWidth="sm">
-        <form onSubmit={createTodo}>
+        <form onSubmit={createTodo} className="add-todo">
           <FormControl fullWidth={true}>
-            <textField
-              label="I have to do this"
+            <TextField
+              label="I will do this"
               variant="standard"
               onChange={handleChange}
               required={true}
@@ -28,6 +38,7 @@ const AddTodo = ({ makeTodos }) => {
               variant="contained"
               color="primary"
               style={{ marginTop: 5 }}
+              type="submit"
             >
               <Add />
               Add
@@ -35,6 +46,22 @@ const AddTodo = ({ makeTodos }) => {
           </FormControl>
         </form>
       </Container>
+      <Snackbar
+        open={open}
+        autoHideDuration={4000}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      >
+        <Alert
+          // icon={<Check fontSize="inherit" />}
+          elevation={6}
+          variant="filled"
+          onClose={() => setOpen(false)}
+          severity="success"
+        >
+          Successfully added item!
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
